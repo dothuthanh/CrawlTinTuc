07a80f1a25ffd7d7e696661862c315ae4ac66485
dothuthanh <55688858+dothuthanh@users.noreply.github.com>
2020-04-16 17:58:55 +0700

    Add files via upload

---
+++ CrawlTinTuc.java
@@ -0,0 +1,28 @@
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class CrawlTinTuc {
+    public static void main(String[] args) {
+        try {
+            URL url = new URL("https://dantri.com.vn/the-gioi.htm");
+            Scanner scanner = new Scanner(new InputStreamReader(url.openStream()));
+            scanner.useDelimiter("\\Z");
+            String content = scanner.next();
+            scanner.close();
+            content = content.replaceAll("\\n+", "");
+            Pattern p = Pattern.compile("alt=\"(.*?)\"");
+            Matcher m = p.matcher(content);
+            while (m.find()) {
+                System.out.println(m.group(1));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
